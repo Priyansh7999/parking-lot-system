40fb5b4c994e77111e3841e3f6489537c29f6645
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-30 13:56:49 +0530

    feat: add CLI flow for admin registration

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Parking Lot System!");
+        System.out.println("Register Admin User");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Admin Name: ");
+        String adminName = scanner.nextLine();
+
+        System.out.print("Enter Admin Mobile Number: ");
+        String adminMobileNumber = scanner.nextLine();
+
+        Admin admin = new Admin(adminName, adminMobileNumber);
+
+        System.out.println("Admin Registered Successfully!");
+        System.out.println(admin.getName() + " - " + admin.getMobileNumber());
     }
 }
