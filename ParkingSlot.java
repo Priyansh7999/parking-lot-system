7ef8973e746765e71e6bafb7a26fbb43efc4e443
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-01-30 14:02:07 +0530

    feat: add ParkingSlot entity

--- ParkingSlot.java
+++ ParkingSlot.java
@@ -1,3 +1,29 @@
+import java.util.UUID;
+
 public class ParkingSlot {
+
+    private final String id;
+    private final SlotType type;
+    private boolean occupied;
+    private String vehicleId;
+
+    public ParkingSlot(SlotType type) {
+        this.id = UUID.randomUUID().toString();
+        this.type = type;
+        this.occupied = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public SlotType getType() {
+        return type;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
     
 }
